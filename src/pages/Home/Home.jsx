bf8494bd348c32bca281d6ff4e3c9e48118c16f5
kamronov <kamronovk3@gmail.com>
2023-05-06 11:25:43 +0500

    badiyat2

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,6 @@ import Slider from '../../components/Slider/Slider'
 import Search from '../../components/Search/Search'
 import Writers from '../../components/Writers/Writers'
 import './Home.scss'
-import Header from '../../components/Header/Header'
 function Home() {
   return (
     <div className='home'>
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Signup from '../../components/Signup/Signup'
 
 function Login() {
   return (
